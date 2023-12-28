ed9e112c5069ed79e24136b3f42e70faaeae48f0
Alin Vlad <opalinvlad04@gmail.com>
2023-12-28 17:23:56 +0200

    Initial commit

---
+++ java/sbur-rest-demo/src/test/java/alv/SburRestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package alv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburRestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
